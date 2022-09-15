cfaeb9d659d5a19bf187acb6ee49f05eb7902ebf
Ruiming Kang <113479175+KANGRUIMING@users.noreply.github.com>
2022-09-15 11:08:36 -0500

    Roadrunner setup
    Setting up roadrunner, attempting to setup roadrunner in our own code.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Autonomous.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+//package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous (name = "name")
+public class Autonomous extends LinearOpMode {
+
+    protected DcMotor frontLeft;
+    protected DcMotor frontRight;
+    protected DcMotor backLeft;
+    protected DcMotor backRight;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        moveForward(1, 1000);
+    }
+    public void moveForward (int power, int time){
+        frontLeft.setPower(power);
+        frontRight.setPower(power);
+        backLeft.setPower(power);
+        backRight.setPower(power);
+        sleep(time);
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+    }
+}
