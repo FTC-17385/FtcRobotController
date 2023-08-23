6344fd1a220692808fe1e3dace92cc0839d63aeb
NotMF <micah.faulkner@mckinneychristian.org>
2023-08-23 07:56:20 -0500

    Create HelloMicah
    Example program from chapter 1.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Faulkner/HelloMicah.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.Faulkner;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class HelloMicah extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addData("Hello","Micah");
+    }
+
+    @Override
+    public void loop() {
+        
+    }
+}
