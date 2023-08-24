94f6b99a0114dfb145fd05cd8ca86c7332a056c1
benn1224 <142230907+benn1224@users.noreply.github.com>
2023-08-24 07:51:21 -0500

    Update HelloBen.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Thomas/HelloBen.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Thomas/HelloBen.java
@@ -1,11 +1,17 @@
 package org.firstinspires.ftc.teamcode.Thomas;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp
-public class HelloBen extends OpMode
+@Autonomous
+public class HelloBen extends OpMode {
+
     @Override
-    public void init()
-        
+    public void init() { telemetry.addData("Hello","Ben"); }
+    @Override
+    public void loop() {
+
+    }
 }
+
