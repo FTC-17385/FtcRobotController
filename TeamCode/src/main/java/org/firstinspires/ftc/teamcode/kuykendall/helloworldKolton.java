421f6daab8a3d743aa75b2ce9abcd5addd869cec
Kolton Kuykendall <kolton.kuykendall@mckinneychristian.org>
2023-08-24 07:32:06 -0500

    fd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kuykendall/helloworldKolton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kuykendall/helloworldKolton.java
@@ -2,14 +2,14 @@ package org.firstinspires.ftc.teamcode.kuykendall;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 @TeleOp()
 public class helloworldKolton extends OpMode {
+    /* This tells the code to be used in Teleop mode, and calls to the file class. */
     @Override
     public void init() {
         telemetry.addData("Hello", "Kolton");
     }
-
+/* This is the initilisatin point of the code */
     @Override
     public void loop() {
 
