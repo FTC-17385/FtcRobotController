922b4e9ec5e96adbb4bf99bd64871d12baae48cb
NotMF <micah.faulkner@mckinneychristian.org>
2023-08-24 07:45:14 -0500

    Update HelloMicah.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Faulkner/HelloMicah.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Faulkner/HelloMicah.java
@@ -1,9 +1,10 @@
 package org.firstinspires.ftc.teamcode.Faulkner;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp
+@Autonomous
 public class HelloMicah extends OpMode {
     @Override
     public void init() {
