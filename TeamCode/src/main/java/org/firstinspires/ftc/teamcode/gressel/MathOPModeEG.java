2341c64ed02c7cbde0418c3284412f2f8c161622
GeorgeCyclops <142230823+GeorgeCyclops@users.noreply.github.com>
2023-08-31 08:30:04 -0500

    Update MathOPModeEG.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gressel/MathOPModeEG.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gressel/MathOPModeEG.java
@@ -12,7 +12,15 @@ public class MathOPModeEG extends OpMode {
     @Override
     public void loop() {
         double speedForward = -gamepad1.left_stick_y / 2.0;
+        double difference = gamepad1.left_stick_y - gamepad1.right_stick_y;
+        double sum = gamepad1.left_trigger + gamepad1.right_trigger;
         telemetry.addData("Left stick y", gamepad1.left_stick_y);
+        telemetry.addData("Right stick x", gamepad1.right_stick_x);
+        telemetry.addData("B button pressed", gamepad1.b);
         telemetry.addData("speed Forward", speedForward);
+        telemetry.addData("Difference",difference);
+        telemetry.addData("Sum", sum);
+
     }
+
 }
