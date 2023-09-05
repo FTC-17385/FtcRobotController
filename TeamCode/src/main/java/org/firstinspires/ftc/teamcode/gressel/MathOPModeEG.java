45554b0cff635f92b6eb94eb0b144e658143d164
GeorgeCyclops <142230823+GeorgeCyclops@users.noreply.github.com>
2023-09-05 07:38:57 -0500

    Update MathOPModeEG.java
    Commented

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gressel/MathOPModeEG.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gressel/MathOPModeEG.java
@@ -11,9 +11,15 @@ public class MathOPModeEG extends OpMode {
     }
     @Override
     public void loop() {
+        /*
+        These are different values that are changed mathematically
+         */
         double speedForward = -gamepad1.left_stick_y / 2.0;
         double difference = gamepad1.left_stick_y - gamepad1.right_stick_y;
         double sum = gamepad1.left_trigger + gamepad1.right_trigger;
+        /*
+        Everything under this adds all of the values to the screen to be seen by the controller
+         */
         telemetry.addData("Left stick y", gamepad1.left_stick_y);
         telemetry.addData("Right stick x", gamepad1.right_stick_x);
         telemetry.addData("B button pressed", gamepad1.b);
