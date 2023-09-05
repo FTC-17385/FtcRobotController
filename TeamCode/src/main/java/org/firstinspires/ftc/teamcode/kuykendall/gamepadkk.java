d33e802eada5505d1a41f0971226652f78d1f6eb
Kolton Kuykendall <kolton.kuykendall@mckinneychristian.org>
2023-09-05 07:35:03 -0500

    Update gamepadkk.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kuykendall/gamepadkk.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kuykendall/gamepadkk.java
@@ -13,5 +13,15 @@ public class gamepadkk extends OpMode {
     double speedForward = -gamepad1.left_stick_y / 2.0;
     telemetry.addData("Left Stick y", gamepad1.left_stick_y);
     telemetry.addData("Speed Forward", speedForward);
+    double Turn = gamepad1.right_stick_x / 2.0;
+    telemetry.addData("Right Stick x", gamepad1.right_stick_x);
+    telemetry.addData("Speed Turn", Turn);
+    double righttrigger = gamepad1.right_trigger / 2.0;
+    telemetry.addData("Right Triggers", gamepad1.right_trigger);
+    telemetry.addData("Trigger Press", righttrigger);
+    double lefttrigger = gamepad1.left_trigger / 2.0;
+    telemetry.addData("Left trigger", gamepad1.left_trigger);
+    telemetry.addData("Left trigger press", lefttrigger);
+
 }
 }
