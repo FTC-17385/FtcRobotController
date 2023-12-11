59394c7c9052ac518a54394f56b85f3245b2d028
Scotthousehold <142230661+ScottHousehold1@users.noreply.github.com>
2023-12-11 08:08:36 -0600

    Bot2 testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot2/DrivePro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot2/DrivePro.java
@@ -2,72 +2,64 @@ package org.firstinspires.ftc.teamcode.Bot2;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-import org.firstinspires.ftc.teamcode.shared.MotionHardware;
 import org.firstinspires.ftc.teamcode.shared.MotionHardware2;
-import org.firstinspires.ftc.teamcode.shared.VisionHardware;
 
 @TeleOp
 public class DrivePro extends LinearOpMode {
-
-    /*private DcMotor frontLeftMotor;
-    private DcMotor frontRightMotor;
-    private DcMotor backLeftMotor;
-    private DcMotor backRightMotor;
-
-    private DcMotor rightLeadScrew;
-    private DcMotor leftLeadScrew;
-    private DcMotor linearExtension;
-
-    private Servo leftIntakeLifter;
-    private Servo rightIntakeLifter;
-    private Servo intakeWrist;
-    private Servo intake;
-    private Servo bucketWrist;
-    private Servo leftClimb;
-    private Servo rightClimb;
-    private Servo planeServo;
-    private FtcDashboard dashboard;
-    public static double INTAKE_WRIST_UP = 1;
-    public static double INTAKE_WRIST_DOWN = 1;
-    public static double INTAKE_LIFTER_UP = 1;
-    public static double INTAKE_LIFTER_DOWN = 1;
-    public static double LEFT_CLIMB_UP_POSITION = 1;
-    public static double LEFT_CLIMB_DOWN_POSITION = 1;
-    public static double RIGHT_CLIMB_UP_POSITION = 1;
-
-    public static double RIGHT_CLIMB_DOWN_POSITION = 1;
-    public static double INTAKE_SPEED = 1;
-    public static double driveScale = .3;
-    public static double strafeScale = .5;
-    public static double rotateScale = .3;
-    public static double FAST_ROTATE_SPEED = 1.0;
-    public static long TURN_180_TIME_MS = 333;
-    private boolean isTurning180 = false;
-    private long turnStartTime = 0;
-    private TelemetryPacket packet;*/
+    private ElapsedTime runtime = new ElapsedTime();
+    private Thread liftThread;
+    private Thread intakeThread;
+    private Thread placementThread;
+    private Thread planeThread;
     MotionHardware2 robot = new MotionHardware2(this);
 
     @Override
     public void runOpMode() {
 
         robot.init();
+        initializeLiftThread();
+        initializeIntakeThread();
+        initializePlacementThread();
+        initializePlaneThread();
 
         waitForStart();
 
+        liftThread.start();
+        intakeThread.start();
+        placementThread.start();
+        planeThread.start();
 
+        telemetry.addData("Mode", "waiting");
 
+        while (opModeIsActive()) {
+            double y = gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = -gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = -gamepad1.right_stick_x;
 
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator; //
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
 
+            robot.frontLeftMotor.setPower(frontLeftPower);
+            robot.backLeftMotor.setPower(backLeftPower);
+            robot.frontRightMotor.setPower(frontRightPower);
+            robot.backRightMotor.setPower(backRightPower);
+            gamepad1.right
+        }
     }
 }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Scott/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Scott/MecanumTeleOp.java
@@ -13,7 +13,7 @@ public class MecanumTeleOp extends LinearOpMode{
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSEs);
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         frontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
